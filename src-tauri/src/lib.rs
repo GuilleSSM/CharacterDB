512466297f38b5e4f85dca07cca48f810865db0c
GuilleSSM <starkilleriv@gmail.com>
2026-01-17 17:44:14 +0100

    Fix updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,42 @@
 use tauri::Manager;
+use tauri_plugin_updater::UpdaterExt;
+use tauri_plugin_log::{Target, TargetKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(tauri_plugin_log::log::LevelFilter::Info)
+                .build(),
+        )
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .target(Target::new(TargetKind::Stdout)) // Still log to terminal
+                .target(Target::new(TargetKind::Webview)) // Send logs to DevTools!
+                .build(),
+        )
         .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                // Manually trigger a check
+                if let Ok(Some(update)) = handle
+                    .updater()
+                    .expect("failed to get updater")
+                    .check()
+                    .await
+                {
+                    // You can then trigger download and install
+                    let _ = update
+                        .download_and_install(|_progress, _chunk| {}, || {})
+                        .await;
+                }
+            });
             #[cfg(debug_assertions)]
             {
                 let window = app.get_webview_window("main").unwrap();
